ef0ce97c8562aa9e1d360689d54244073b3f6c22
Baptiste Pignol <pignol.baptiste@gmail.com>
2015-05-20 16:00:48 +0200

    add drawing services

--- server/main.js
+++ server/main.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 http.listen(3000, function(){
     console.log('listening on *:3000');
     DrawingService.create("test");
-});});
+});
 
 var rtmServer = new RtmServer(io);
 
